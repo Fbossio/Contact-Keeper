0b8979178cea8ca63223d2f48cdd2e269d533146
Felix Bossio <felixbossio@gmail.com>
2020-10-26 02:54:09 -0500

    Configure connection with the database

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+// Connect database
+
+connectDB();
 
 app.get('/', (req, res) => {
   res.json({ msg: 'Welcome to the Contact Keeper API...' });
@@ -10,6 +14,8 @@ app.get('/', (req, res) => {
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}!`);
