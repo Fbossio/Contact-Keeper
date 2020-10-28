28372e6df450b2e779a9143a8d35d41e203191f3
Felix Bossio <felixbossio@gmail.com>
2020-10-28 15:37:15 -0500

    Setup client side and start building contacts components

---
+++ client/src/components/contacts/ContactForm.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const ContactForm = () => {
+  const [contact, setContact] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    type: 'personal',
+  });
+
+  const { name, email, phone, type } = contact;
+  return (
+    <form>
+      <h2 className="text-primary">Add Contact</h2>
+      <input
+        type="text"
+        placeholder="Name"
+        name="name"
+        value={name}
+        onChange={handleChange}
+      />
+    </form>
+  );
+};
+
+export default ContactForm;
