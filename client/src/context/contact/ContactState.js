211b1fb6a9f5304e757fbb194a7739e9b9a31c3a
Felix Bossio <felixbossio@gmail.com>
2020-10-31 00:51:39 -0500

    Update contact functionality

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -60,6 +60,26 @@ const ContactState = (props) => {
     }
   };
 
+  // Update contact
+  const updateContact = async (contact) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+      dispatch({ type: UPDATE_CONTACT, payload: res.data });
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+    }
+  };
+
   // Clear contacts
   const clearContacts = () => {
     dispatch({ type: CLEAR_CONTACTS });
@@ -73,10 +93,7 @@ const ContactState = (props) => {
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
   };
-  // Update contact
-  const updateContact = (contact) => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
-  };
+
   // Filter contacts
   const filterContacts = (text) => {
     dispatch({ type: FILTER_CONTACTS, payload: text });
