d93f0ec188a2ec42c4b7189dbec45aeee1b76137
Felix Bossio <felixbossio@gmail.com>
2020-10-31 00:26:22 -0500

    Get contacts and clear contacts functionalities

--- client/src/components/layout/NavBar.jsx
+++ client/src/components/layout/NavBar.jsx
@@ -1,15 +1,19 @@
 import React, { Fragment, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import AuthContext from './../../context/auth/authContext';
+import ContactContext from './../../context/contact/contactContext';
 import PropTypes from 'prop-types';
 
 const NavBar = ({ title, icon }) => {
   const authContext = useContext(AuthContext);
+  const contactContext = useContext(ContactContext);
 
   const { isAuthenticated, logout, user } = authContext;
+  const { clearContacts } = contactContext;
 
   const handleLogout = () => {
     logout();
+    clearContacts();
   };
 
   const authLinks = (
